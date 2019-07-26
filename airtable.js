45fdf47f4fc349d4d17e3cb8446dc357a0cdbd88
Fredward <faandrade@gmail.com>
2019-07-25 22:36:18 -0400

    remove records folder, update readme and method

--- airtable.js
+++ airtable.js
@@ -4,17 +4,35 @@ require('dotenv').config();
 
 var base = new Airtable({apiKey: process.env.APIKEY}).base(process.env.BASE);
 
-
 base('🍩 Oral Histories').select({
     // Selecting the first 3 records in Worksheet:
-    maxRecords: 3,
-    view: "Main View"
+    // maxRecords: 3,
+    view: ".LOCMetadataView",
+    cellFormat: "string",
+    timeZone: "America/New_York",
+    userLocale: "en-ca",
+    fields: [
+      "IDv2",
+      "Languages by ISO Code",
+      "Languages Used",
+      "Alternate Name",
+      "Speakers",
+      ".self?",
+      "Source",
+      "Video Nation",
+      "Video Territory",
+      "Video Description",
+      "Licenses",
+      "Youtube Publish Schedule",
+      "Wikimedia Status",
+      "Wiki Commons URL"
+    ]
 }).eachPage(function page(records, fetchNextPage) {
     // This function (`page`) will get called for each page of records.
 
     records.forEach(function(record) {
         const stringifedJSON = JSON.stringify(record);
-        fs.writeFileSync(`test/${record.get('IDv2')}.json`, stringifedJSON);
+        fs.writeFileSync(`dump/${record.get('IDv2')}/${record.get('IDv2')}__metadata.json`, stringifedJSON);
         console.log('wrote', record.get('IDv2'));
     });
 
